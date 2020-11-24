a41877b9c9074b5099875829f0099aaeeb1ead6e
Anton <darkneverdie@mail.ru>
2020-11-24 13:12:35 +0700

    poity obyedat

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
+import sqlite3
 from datetime import datetime
 from config import db, ma
 
 class Person(db.Model):
   __tablename__ = 'person'
   person_id = db.Column(db.Integer, primary_key=True)
-  lname = db.Column(db.String(32), index=True)
-  fname = db.Column(db.String(32))
+  lname = db.Column(db.String)
+  fname = db.Column(db.String)
   timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 class PersonSchema(ma.SQLAlchemyAutoSchema):
